70d65ea8f0a97631c6bde34782bfc55c4ee91549
aydrienlaw <adrianzelaw@gmail.com>
2025-08-28 15:06:28 +0800

    Tilo: Add sayHi, sayGoodbye, printBorder methods

--- src/main/java/Tilo.java
+++ src/main/java/Tilo.java
@@ -1,11 +1,30 @@
 public class Tilo {
+    public static final String BORDER = "----------------------------------------";
+    public static final String LOGO = "___________.___.____     ________ \n"
+            + "\\__    ___/|   |    |   \\_____  \\\n"
+            + "   |    |  |   |    |    /   |   \\\n"
+            + "   |    |  |   |    |___/    |    \\\n"
+            + "   |____|  |___|_______ \\_______  /\n"
+            + "                       \\/       \\/";
+
+    public static void printBorder() {
+        System.out.println(BORDER);
+    }
+
+    public static void sayHi() {
+        String greetMsg = "Hello! I'm Tilo\n"
+                + "What can I do for you?";
+        System.out.println(LOGO + "\n" + greetMsg);
+        printBorder();
+    }
+
+    public static void sayGoodbye() {
+        System.out.println("Bye. Hope to see you again soon!");
+        printBorder();
+    }
+
     public static void main(String[] args) {
-        String logo = "___________.___.____     ________ \n"
-                + "\\__    ___/|   |    |   \\_____  \\\n"
-                + "   |    |  |   |    |    /   |   \\\n"
-                + "   |    |  |   |    |___/    |    \\\n"
-                + "   |____|  |___|_______ \\_______  /\n"
-                + "                       \\/       \\/ \n";
-        System.out.println("Hello from\n" + logo);
+        sayHi();
+        sayGoodbye();
     }
 }
