7fecd1ce18e4557c917ca51755c1559c7c7ddf04
aydrienlaw <adrianzelaw@gmail.com>
2025-09-14 18:45:02 +0800

    TiloException: Add new exception cases

--- src/main/java/tilo/exception/TiloException.java
+++ src/main/java/tilo/exception/TiloException.java
@@ -5,6 +5,42 @@ public class TiloException extends Exception {
         super(message);
     }
 
+    public static TiloException noCommand() {
+        return new TiloException("Please enter a command.");
+    }
+
+    public static TiloException invalidCommand() {
+        return new TiloException("Please enter a valid command.");
+    }
+
+    public static TiloException emptyTaskDescription(String task) {
+        return new TiloException("The description of a " + task + " cannot be empty.");
+    }
+
+    public static TiloException invalidDeadlineFormat() {
+        return new TiloException("Deadline format should be: deadline <description> /by <date>");
+    }
+
+    public static TiloException emptyDeadlineBy() {
+        return new TiloException("The deadline date cannot be empty.");
+    }
+
+    public static TiloException invalidEventFormat() {
+        return new TiloException("Event format should be: event <description> /from <start date> /to <end date>");
+    }
+
+    public static TiloException emptyEventFrom() {
+        return new TiloException("The event start date cannot be empty.");
+    }
+
+    public static TiloException emptyEventTo() {
+        return new TiloException("The event end date cannot be empty.");
+    }
+
+    public static TiloException noTaskNumber(String operation) {
+        return new TiloException("Please specify a task number to " + operation + ".");
+    }
+
     public static TiloException invalidTaskNumber() {
         return new TiloException("Please enter a valid task number.");
     }
